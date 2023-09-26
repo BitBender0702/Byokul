5b925e72691e7a0f90418c74fc6d30c0112ab738
admin <admin@DESKTOP-4NONQBE>
2023-09-26 09:55:50 +0530

    condition change 26/09/2023

--- LMS.App/ClientApp/src/environments/environment.ts
+++ LMS.App/ClientApp/src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'https://localhost:7220',
-  // apiUrl: 'https://byokul.com'
+  // apiUrl: 'https://localhost:7220',
+  apiUrl: 'https://byokul.com'
 };
 
 /*yy
